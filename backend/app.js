49fe186003639dd24be264936ba3cd58dbb1ef3d
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2022-08-06 15:40:29 +0200

    CRUD Finished

--- backend/app.js
+++ backend/app.js
@@ -5,7 +5,11 @@ const mongoose = require('mongoose');
 const sauce = require('./models/sauce');
 
 
-
+mongoose.connect('mongodb+srv://AmrDevProjetSix:password@clusterhottakes.6vzg70w.mongodb.net/?retryWrites=true&w=majority',
+{ useNewUrlParser: true,
+    useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
     
     
     const app = express();
@@ -27,6 +31,19 @@ app.use((req, res, next) => {
     .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
     .catch(error => res.status(400).json({ error }));
   });
+
+app.put('/api/stuff/:id', (req, res, next) => { 
+    sauce.updateOne({ _id: req.params.id }, { ...req.body, _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet mis à jour !' }))
+    .catch(error => res.status(400).json({ error }));  
+});
+
+app.delete('/api/stuff/:id', (req, res, next) => { 
+    sauce.deleteOne({ _id: req.params.id })
+    .then(() => res.status(200).json({ message: 'Objet supprimé !' }))
+    .catch(error => res.status(400).json({ error }));  
+}); 
+
 
 app.get('/api/stuff/:id', (req, res, next) => { 
     sauce.findOne({ _id: req.params.id })
