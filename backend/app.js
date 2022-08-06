11795a755dfe4f0e6cfb073b58a88f2a097f2337
Abd El Fattah Amr <amrelfattah.dev@gmail.com>
2022-08-06 15:00:10 +0200

    Create a schema of data with Mongoose

--- backend/app.js
+++ backend/app.js
@@ -1,15 +1,17 @@
 const express = require('express');
-
-const app = express();
-
+const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
+const sauce = require('./models/sauce');
 
 
 
+    
+    
+    const app = express();
 app.use(express.json());
 
-app.get((req, res, next) => {
+app.use((req, res, next) => {
     res.setHeader('Access-Control-Allow-Origin', '*');
     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
@@ -17,32 +19,26 @@ app.get((req, res, next) => {
   });
 
   app.post('/api/stuff', (req, res, next) => {
-    console.log(req.body);
-    res.status(201).json({
-      message: 'Objet créé !'
+    delete req.body._id;
+    const sauce = new sauce({ 
+        ...req.body,
     });
+    sauce.save() 
+    .then(() => res.status(201).json({ message: 'Objet enregistré !' }))
+    .catch(error => res.status(400).json({ error }));
   });
 
+app.get('/api/stuff/:id', (req, res, next) => { 
+    sauce.findOne({ _id: req.params.id })
+    .then(sauce => res.status(200).json({ sauce }))
+    .catch(error => res.status(400).json({ error }));
+    });  
+
+
 app.get('/api/stuff', (req, res, next) => {
-    const stuff = [
-      {
-        _id: 'oeihfzeoi',
-        title: 'Mon premier objet',
-        description: 'Les infos de mon premier objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 4900,
-        userId: 'qsomihvqios',
-      },
-      {
-        _id: 'oeihfzeomoihi',
-        title: 'Mon deuxième objet',
-        description: 'Les infos de mon deuxième objet',
-        imageUrl: 'https://cdn.pixabay.com/photo/2019/06/11/18/56/camera-4267692_1280.jpg',
-        price: 2900,
-        userId: 'qsomihvqios',
-      },
-    ];
-    res.status(200).json(stuff);
+    sauce.find()
+    .then(sauces => res.status(200).json({ sauces }))
+    .catch(error => res.status(400).json({ error }));
   });
 
 module.exports = app;
